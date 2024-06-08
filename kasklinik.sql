049c10029ecedae76f17c29febd4a261767d7554
Afariphi <agung.afr10@gmail.com>
2024-06-08 16:00:51 +0700

    db updated

--- kasklinik.sql
+++ kasklinik.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 07, 2024 at 12:41 PM
+-- Generation Time: Jun 08, 2024 at 11:00 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -24,143 +24,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `action`
+-- Table structure for table `balance`
 --
 
-CREATE TABLE `action` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `patient_id` bigint(20) UNSIGNED NOT NULL,
-  `notes` text NOT NULL,
-  `diagnosis` text NOT NULL,
-  `medicine` text NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `patient`
---
-
-CREATE TABLE `patient` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `fullname` varchar(255) NOT NULL,
-  `address` varchar(255) NOT NULL,
-  `phone` varchar(255) NOT NULL,
-  `category` varchar(255) NOT NULL,
-  `assurance` varchar(255) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `transaction`
---
-
-CREATE TABLE `transaction` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `transaction_in_id` varchar(255) DEFAULT NULL,
-  `transaction_out_id` varchar(255) DEFAULT NULL,
-  `type` varchar(255) NOT NULL,
-  `suppliers` varchar(255) NOT NULL,
-  `comment` text NOT NULL,
-  `price` double NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `transaction_in`
---
-
-CREATE TABLE `transaction_in` (
-  `id` varchar(255) NOT NULL,
-  `action_id` bigint(20) UNSIGNED NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `doctor` varchar(255) NOT NULL,
-  `total_price` double NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `transaction_out`
---
-
-CREATE TABLE `transaction_out` (
-  `id` varchar(255) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `information` text NOT NULL,
-  `total_price` double NOT NULL,
-  `suppliers` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
-
-CREATE TABLE `user` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `fullname` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
+CREATE TABLE `balance` (
+  `id` int(11) NOT NULL,
+  `balance` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `user`
+-- Dumping data for table `balance`
 --
 
-INSERT INTO `user` (`id`, `username`, `fullname`, `password`) VALUES
-(1, 'admin', 'admin', '$2y$10$gtfIGqLpyzLH1Ed9LWgEr.dSVTMUOS/6llUQ9DZaKMO38DlaonfkG');
+INSERT INTO `balance` (`id`, `balance`) VALUES
+(1, 100000000);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `action`
+-- Indexes for table `balance`
 --
-ALTER TABLE `action`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `patient_id` (`patient_id`);
-
---
--- Indexes for table `patient`
---
-ALTER TABLE `patient`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `transaction`
---
-ALTER TABLE `transaction`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `transaction_id` (`transaction_in_id`),
-  ADD KEY `fk_transaction_transaction_out_id` (`transaction_out_id`);
-
---
--- Indexes for table `transaction_in`
---
-ALTER TABLE `transaction_in`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id` (`id`),
-  ADD KEY `transaction_in_action_id_foreign` (`action_id`);
-
---
--- Indexes for table `transaction_out`
---
-ALTER TABLE `transaction_out`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id` (`id`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
+ALTER TABLE `balance`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -168,46 +54,10 @@ ALTER TABLE `user`
 --
 
 --
--- AUTO_INCREMENT for table `action`
+-- AUTO_INCREMENT for table `balance`
 --
-ALTER TABLE `action`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
-
---
--- AUTO_INCREMENT for table `patient`
---
-ALTER TABLE `patient`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
-
---
--- AUTO_INCREMENT for table `transaction`
---
-ALTER TABLE `transaction`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63073062;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `action`
---
-ALTER TABLE `action`
-  ADD CONSTRAINT `action_ibfk_1` FOREIGN KEY (`patient_id`) REFERENCES `patient` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `transaction`
---
-ALTER TABLE `transaction`
-  ADD CONSTRAINT `fk_transaction_transaction_out_id` FOREIGN KEY (`transaction_out_id`) REFERENCES `transaction_out` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `transaction_ibfk_1` FOREIGN KEY (`transaction_in_id`) REFERENCES `transaction_in` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `transaction_transaction_id_foreign_in` FOREIGN KEY (`transaction_in_id`) REFERENCES `transaction_in` (`id`);
-
---
--- Constraints for table `transaction_in`
---
-ALTER TABLE `transaction_in`
-  ADD CONSTRAINT `transaction_in_action_id_foreign` FOREIGN KEY (`action_id`) REFERENCES `action` (`id`) ON DELETE CASCADE;
+ALTER TABLE `balance`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
