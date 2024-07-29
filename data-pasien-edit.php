d8ddf6e4d715ecd12c9605f156cb294f747e4c38
Angga Suriana <anggasurianae65@gmail.com>
2024-07-29 00:28:20 +0000

    debug

--- data-pasien-edit.php
+++ data-pasien-edit.php
@@ -183,6 +183,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   <script src="assets/plugins/chart.js/chart.min.js"></script>
   <script src="assets/js/index-charts.js"></script>
   <script src="assets/js/app.js"></script>
+  <script>
+    alert("<?php echo $message ?>");
+  </script>
 </body>
 
 </html>
