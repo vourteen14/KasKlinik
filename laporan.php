7545cf3212f8494ec61e5309f943311584f2d4cd
Angga Suriana <anggasurianae65@gmail.com>
2024-08-01 02:14:37 +0000

    debug laporan

--- laporan.php
+++ laporan.php
@@ -222,7 +222,7 @@ $offset = ($page - 1) * $itemsPerPage; // Menghitung offset untuk nomor baris
 
 																if(htmlspecialchars($data1['category']) == "Asuransi" || htmlspecialchars($data1['category']) == "BPJS") { ?>
 																<?php ?>
-																<button class="ms-1 btn app-btn-primary" onclick="generateBilling(<?php echo $data2; ?>)">Tagihan</button>
+																<button class="ms-1 btn app-btn-primary" onclick='generateBilling(<?php echo $data2; ?>)'>Tagihan</button>
 															<?php }}; ?>
 														</div>
 													</td>
