dff196a30b57afeeeb150be2277f3f724ff29847
Angga Suriana <anggasurianae65@gmail.com>
2024-08-01 08:55:54 +0000

    fix everything

--- data-pasien-input.php
+++ data-pasien-input.php
@@ -154,7 +154,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 <label class="form-label" for="nomorasuransi">Nomor Asuransi/BPJS (Optional untuk Umum)</label>
                 <input id="nomorasuransi" name="nomorasuransi" type="text" class="form-control">
               </div>
-              <div class="pt-2">
+              <div class="pt-3">
                 <button type="submit" class="btn app-btn-primary w-100 theme-btn mx-auto">
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2" />
