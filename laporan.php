6bb046ae93016236e31364669cfc8659fc989d72
Angga Suriana <anggasurianae65@gmail.com>
2024-08-01 09:53:03 +0000

    fix everything

--- laporan.php
+++ laporan.php
@@ -559,7 +559,7 @@ $offset = ($page - 1) * $itemsPerPage; // Menghitung offset untuk nomor baris
 			billingWindow.document.write('<div class="header">');
 			billingWindow.document.write('<p><span class="label">Tipe Asuransi:</span><span class="value">&nbsp;&nbsp;&nbsp;' + assType + '</span></p>');
 			billingWindow.document.write('<p><span class="label">Nomor Asuransi:</span><span class="value">&nbsp;&nbsp;&nbsp;' + assNumber + '</span></p>');
-			billingWindow.document.write('<p><span class="label">Tipe Asuransi:</span><span class="value">&nbsp;&nbsp;&nbsp;' + index.category + '</span></p>');
+			billingWindow.document.write('<p><span class="label">Pembayaran:</span><span class="value">&nbsp;&nbsp;&nbsp;' + index.category + '</span></p>');
 			billingWindow.document.write('<p><span class="label">Nama Lengkap:</span><span class="value">&nbsp;&nbsp;&nbsp;' + index.fullname + '</span></p>');
 			billingWindow.document.write('<p><span class="label">Alamat:</span><span class="value">&nbsp;&nbsp;&nbsp;' + index.address + '</span></p>');
 			billingWindow.document.write('<p><span class="label">Telepon:</span><span class="value">&nbsp;&nbsp;&nbsp;' + index.phone + '</span></p>');
