1c71e7894ba9a86f06ebc93824898faa2ecfec21
Angga Suriana <anggasurianae65@gmail.com>
2024-08-01 06:22:18 +0000

    fix laporan

--- laporan.php
+++ laporan.php
@@ -161,24 +161,20 @@ $offset = ($page - 1) * $itemsPerPage; // Menghitung offset untuk nomor baris
 								<div class="app-card app-card-stat shadow-sm h-100">
 									<div class="app-card-body p-2 p-lg-2">
 										<h4 class="stats-type mb-2">Pasien</h4>
-										<?php
-										$sql = "SELECT balance FROM patient WHERE id = 1";
-										$result = $conn->query($sql);
+											<?php
+											$sql = "SELECT COUNT(id) AS total FROM patient";
+											$result = $conn->query($sql);
 
-										if ($result->rowCount() > 0) {
-											$row = $result->fetch(PDO::FETCH_ASSOC);
-											$balance = $row["balance"];
-											if ($balance >= 1000000) {
-												echo "<div class=\"stats-figure\">" . number_format($balance / 1000000, 1, ',', '.') . " juta</div>";
-											} elseif ($balance >= 1000) {
-												echo "<div class=\"stats-figure\">" . round($balance / 1000) . " ribu</div>";
+											if ($result->rowCount() > 0) {
+												// Mengambil data dari setiap baris
+												while ($row = $result->fetch()) {
+													$total = $row["total"];
+													echo "<div class=\"stats-figure\">" . $total . "</div>";
+												}
 											} else {
-												echo "<div class=\"stats-figure\">" . $balance . "</div>";
+												echo "0 results";
 											}
-										} else {
-											echo "<div class=\"stats-figure\">0</div>";
-										}
-										?>
+											?>
 										<div class="stats-meta text-success">Rupiah</div>
 									</div>
 									<a class="app-card-link-mask" href="#"></a>
